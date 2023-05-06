c18a47a39159ec96a63d80dc37cf0599560d6191
Luis Roberto Torres Castillo <luistorresdev@gmail.com>
2023-05-05 23:39:13 -0600

    fourth commitment, player walk animation

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rigidBody2d;
+    [SerializeField] Animator animator;
 
     [SerializeField] float speed;
     private Vector2 movementInput;
@@ -16,11 +17,29 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         rigidBody2d.velocity = movementInput * speed * Time.deltaTime;
+        AnimateMovement(movementInput);
     }
 
     //Called whenever there is a change to an input bound to the Move action
     private void OnMove(InputValue inputValue)
     {
         movementInput = inputValue.Get<Vector2>();
+    }
+
+    private void AnimateMovement(Vector2 direction)
+    {
+        if (animator != null)
+        {
+            if(direction.magnitude > 0)
+            {
+                animator.SetBool("isMoving", true);
+                animator.SetFloat("horizontal", direction.x);
+                animator.SetFloat("vertical", direction.y);
+            }
+            else
+            {
+                animator.SetBool("isMoving", false);
+            }
+        }
     }
 }
